4db170271199ee66cdbac048e1cbc60707df86c2
redenv <redenv@careindeed.com>
2023-06-09 19:43:01 +0800

    sample

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,67 @@
+'use client'
+
+import { useEffect, useState } from "react";
 import getData from "./api/GetCryptoInfo";
 
 type coin = {
   symbol: string;
   price: number;
 };
-export default async function Page() {
-  const data: coin[] = await getData(["BNBUSDT", "BTCUSDT", "ETHUSDT"]);
-  console.log(data);
+export default function Page() {
+  const [coins, setCoins] = useState(['BTCUSDT', 'ETHUSDT', 'BNBUSDT']);
+  const [coinData, setCoinData] = useState<coin[]>([]);
+  const [oldCoinData, setOldCoinData] = useState<coin[]>([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const getCoinData = async () => {
+        setOldCoinData(coinData);
+        const data = await getData(coins);
+        setCoinData(data);
+      };
+      getCoinData();
+    }, 5000);
+  });
+
+  const getOldCoin = (symbol: string) => {
+    return oldCoinData.find(c => c.symbol === symbol) ?? { symbol: symbol, price: 0 };
+  };
+
+  const getCoinColor = (symbol: string) => {
+    const oldCoin = getOldCoin(symbol);
+    const newCoin = coinData.find(c => c.symbol === symbol) ?? { symbol: symbol, price: 0 };
+    return oldCoin.price > newCoin.price ? 'text-red' : 'text-green';
+  };
+
+  const getCoinDiff = (symbol: string) => {
+    const oldCoin = getOldCoin(symbol);
+    const newCoin = coinData.find(c => c.symbol === symbol) ?? { symbol: symbol, price: 0 };
+    return oldCoin.price - newCoin.price;
+  };
+
+  const getCoinPricePercentage = (symbol: string) => {
+    const oldCoin = getOldCoin(symbol);
+    const newCoin = coinData.find(c => c.symbol === symbol) ?? { symbol: symbol, price: 0 };
+    return (oldCoin.price - newCoin.price) / oldCoin.price;
+  };
+
   return (
-    <main>
-      <h1 className="text-white">Tanga</h1>
-    </main>
+    <main className="flex flex-col gap-5">
+      <textarea className="h-12" onChange={
+        (e) => {
+          setCoins(e.target.value.trim().split('\n'));
+        }
+      } ></textarea>
+      {
+        coinData.map((coin) => (
+          <div className="flex flex-row gap-2" key={coin.symbol}>
+            <h2 className="font-bold text-white">{coin.symbol}</h2>
+            <p className={`${getCoinColor(coin.symbol)}`}>Current Price: {coin.price}</p>
+            <p className={`${getCoinColor(coin.symbol)}`}>Price Diff: {getCoinDiff(coin.symbol).toFixed(5)}</p>
+            <p className={`${getCoinColor(coin.symbol)}`}>Percentage: {getCoinPricePercentage(coin.symbol).toFixed(5)}%</p>
+          </div>
+        ))
+      }
+    </main >
   );
 }
